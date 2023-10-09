433d1e3851bfcf1c8c435f62c0296874211f7f05
kirychukyurii <me@kirychuk.pp.ua>
2023-10-09 19:18:58 +0300

    test: add testcases for grafana client

--- pkg/infra/grafana/grafana.go
+++ pkg/infra/grafana/grafana.go
@@ -8,6 +8,8 @@ import (
 	"fmt"
 	"github.com/kirychukyurii/grafana-reporter-plugin/pkg/apperrors"
 	"net/http"
+	"net/url"
+	"path"
 	"strconv"
 	"time"
 
@@ -19,10 +21,16 @@ import (
 // Client is a Grafana API client.
 type Client struct {
 	setting    *config.ReporterAppConfig
+	baseURL    *url.URL
 	connection *fasthttp.Client
 }
 
 func New(setting *config.ReporterAppConfig) (*Client, error) {
+	u, err := url.Parse(setting.GrafanaConfig.URL)
+	if err != nil {
+		return nil, err
+	}
+
 	cli := fasthttp.Client{
 		TLSConfig: &tls.Config{
 			InsecureSkipVerify: setting.GrafanaConfig.InsecureSkipVerify,
@@ -31,15 +39,15 @@ func New(setting *config.ReporterAppConfig) (*Client, error) {
 
 	return &Client{
 		setting:    setting,
+		baseURL:    u,
 		connection: &cli,
 	}, nil
 }
 
-func (c *Client) Request(ctx context.Context, requestMethod, requestUrl string, requestPayload any, responseStruct any) error {
+func (c *Client) Request(ctx context.Context, requestMethod, requestPath string, requestPayload any, responseStruct any) error {
 	var (
-		err  error
-		resp *fasthttp.Response
-		body []byte
+		err         error
+		shouldRetry bool
 	)
 
 	retryStatusCodes := c.setting.GrafanaConfig.RetryStatusCodesArr()
@@ -54,22 +62,14 @@ func (c *Client) Request(ctx context.Context, requestMethod, requestUrl string, 
 			if c.setting.GrafanaConfig.RetryTimeout == 0 {
 				c.setting.GrafanaConfig.RetryTimeout = time.Second * 5
 			}
-			time.Sleep(c.setting.GrafanaConfig.RetryTimeout)
+
+			time.Sleep(time.Second * c.setting.GrafanaConfig.RetryTimeout)
 		}
 
 		// If err is not nil, retry again
 		// That's either caused by client policy, or failure to speak HTTP (such as network connectivity problem). A
 		// non-2xx status code doesn't cause an error.
-		resp, err = c.newRequest(requestMethod, requestUrl, requestPayload)
-		if err != nil {
-			continue
-		}
-
-		shouldRetry, err := matchRetryCode(resp.StatusCode(), retryStatusCodes)
-		if err != nil {
-			return err
-		}
-
+		shouldRetry, err = c.newRequest(retryStatusCodes, requestMethod, requestPath, requestPayload, responseStruct)
 		if !shouldRetry {
 			break
 		}
@@ -79,41 +79,21 @@ func (c *Client) Request(ctx context.Context, requestMethod, requestUrl string, 
 		return err
 	}
 
-	// do we need to decompress the response?
-	contentEncoding := resp.Header.Peek("Content-Encoding")
-	if bytes.EqualFold(contentEncoding, []byte("gzip")) {
-		body, err = resp.BodyGunzip()
-		if err != nil {
-			return fmt.Errorf("decompress the response: %v", err)
-		}
-	} else {
-		body = resp.Body()
-	}
-
-	switch {
-	case resp.StatusCode() == http.StatusNotFound:
-		return fmt.Errorf("%v, body: %s", apperrors.ErrObjectNotFound, string(body))
-
-	case resp.StatusCode() >= 400:
-		return fmt.Errorf("expected status code %d but got %d, response: %s", fasthttp.StatusOK, resp.StatusCode(), string(body))
-	}
-
-	if responseStruct == nil {
-		return nil
-	}
-
-	if err = json.Unmarshal(body, responseStruct); err != nil {
-		return err
-	}
-
 	return nil
 }
 
-func (c *Client) newRequest(requestMethod, requestUrl string, requestPayload any) (*fasthttp.Response, error) {
+func (c *Client) newRequest(retryStatusCodes []string, requestMethod, requestPath string, requestPayload any, responseStruct any) (bool, error) {
+	var (
+		err  error
+		body []byte
+	)
+
 	req := fasthttp.AcquireRequest()
 	defer fasthttp.ReleaseRequest(req)
 
-	req.SetRequestURI(requestUrl)
+	u := c.baseURL
+	u.Path = path.Join(u.Path, requestPath)
+	req.SetRequestURI(u.String())
 	req.Header.SetMethod(requestMethod)
 	req.Header.Set("Accept", "application/json")
 
@@ -132,11 +112,46 @@ func (c *Client) newRequest(requestMethod, requestUrl string, requestPayload any
 	resp := fasthttp.AcquireResponse()
 	defer fasthttp.ReleaseResponse(resp)
 
-	if err := c.connection.Do(req, resp); err != nil {
-		return nil, fmt.Errorf("client get failed: %v", err)
+	if err = c.connection.Do(req, resp); err != nil {
+		return false, fmt.Errorf("client %s get failed: %v", req.RequestURI(), err)
 	}
 
-	return resp, nil
+	shouldRetry, err := matchRetryCode(resp.StatusCode(), retryStatusCodes)
+	if err != nil {
+		return false, err
+	}
+
+	if !shouldRetry {
+
+		// do we need to decompress the response?
+		contentEncoding := resp.Header.Peek("Content-Encoding")
+		if bytes.EqualFold(contentEncoding, []byte("gzip")) {
+			body, err = resp.BodyGunzip()
+			if err != nil {
+				return false, fmt.Errorf("decompress the response: %v", err)
+			}
+		} else {
+			body = resp.Body()
+		}
+
+		switch {
+		case resp.StatusCode() == http.StatusNotFound:
+			return false, fmt.Errorf("%v, body: %s", apperrors.ErrObjectNotFound, string(body))
+
+		case resp.StatusCode() >= 400:
+			return false, fmt.Errorf("expected status code %d but got %d, response: %s", fasthttp.StatusOK, resp.StatusCode(), string(body))
+		}
+
+		if responseStruct == nil {
+			return false, nil
+		}
+
+		if err = json.Unmarshal(body, &responseStruct); err != nil {
+			return false, fmt.Errorf("unmarshal json (%s): %v", body, err)
+		}
+	}
+
+	return shouldRetry, nil
 }
 
 // matchRetryCode checks if the status code matches any of the configured retry status codes.
